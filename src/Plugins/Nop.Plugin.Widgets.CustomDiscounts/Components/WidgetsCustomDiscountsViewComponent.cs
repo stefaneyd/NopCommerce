2dbe5a6a628a70e316349325a0b4b904272ee545
stefan@ozio.is <stefan@ozio.is>
2019-06-27 22:42:24 +0000

    Task 2, initial check-in

---
+++ src/Plugins/Nop.Plugin.Widgets.CustomDiscounts/Components/WidgetsCustomDiscountsViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Framework.Components;
+
+namespace Nop.Plugin.Widgets.CustomDiscounts.Components
+{
+    [ViewComponent(Name = "WidgetsCustomDiscounts")]
+    public class WidgetsCustomDiscountsViewComponent : NopViewComponent
+    {
+        public WidgetsCustomDiscountsViewComponent()
+        {
+
+        }
+
+        public IViewComponentResult Invoke(string widgetZone, object additionalData)
+        {
+            //return Content("<h2>50% Discount only this month</h2>");
+            return View("~/Plugins/Widgets.CustomDiscounts/Views/CustomDiscounts.cshtml");
+        }
+    }
+}
---
+++ src/Plugins/Nop.Plugin.Widgets.CustomDiscounts/CustomDiscountsPlugin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Nop.Services.Cms;
+using Nop.Services.Plugins;
+using Nop.Web.Framework.Infrastructure;
+
+namespace Nop.Plugin.Widgets.CustomDiscounts
+{
+    public class CustomDiscountsPlugin : BasePlugin, IWidgetPlugin
+    {
+        public bool HideInWidgetList => false;
+
+        public string GetWidgetViewComponentName(string widgetZone)
+        {
+            return "WidgetsCustomDiscounts";
+        }
+
+        public IList<string> GetWidgetZones()
+        {
+            return new List<string> { PublicWidgetZones.ProductDetailsOverviewTop };
+            //return new List<string> { "productdetails_add_info", "productreviews_page_top", "productreviews_page_inside_review", "productreviews_page_bottom", "productdetails_top", "productdetails_overview_top", "productdetails_overview_bottom", "productdetails_inside_overview_buttons_before", "productdetails_inside_overview_buttons_after", "productdetails_essential_top", "productdetails_essential_bottom", "productdetails_bottom", "productdetails_before_pictures", "productdetails_before_collateral", "productdetails_after_pictures", "productdetails_after_breadcrumb", "your_custom_widget_zone" };
+        }
+    }
+}
